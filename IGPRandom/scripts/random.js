181d426f3be88736a22c64f3a7249fe827cdab37
Stephen Rice <mail@stephenrice.co.uk>
2024-11-21 14:20:23 +0000

    Add missing blogpost, images and IGPRandom

---
+++ IGPRandom/scripts/random.js
@@ -0,0 +1,247 @@
+
+const playstyles = ["Turtle",
+                    "Pressure",
+                    "Mega Expand",
+                    "Proxy Base",
+                    "Seige",
+                    "One Base",
+                    "Be Sneaky",
+                    "Deathball",
+                    "Pyre Greed"];
+const factions = ["ARU",
+                  "QRATH"];
+const qrathImmortals = ["ORZUM",
+                        "AJARI"];
+const aruImmortals = ["MALA",
+                      "XOL"];
+const malaUnits = ["Masked Hunter",
+                   "Xacal",
+                   "Ichor",
+                   "Bloodbound",
+                   "Dread Sister",
+                   "Brood Anchor",
+                   "Wraith Bow",
+                   "Incubator",
+                   "Thrum",
+                   "Aarox",
+                   "Behemoth"];     
+const xolUnits = ["Bone Stalker",
+                  "Xacal",
+                  "Underspine",
+                  "Ichor",
+                  "White Wood Reaper",
+                  "Red Seer",
+                  "Thrum",
+                  "Aarox",
+                  "Behemoth",
+                  "Brood Anchor",
+                  "Wraith Bow"];
+const orzumUnits = ["Zentari",
+                    "Zephyr",
+                    "Magi",
+                    "Dervish",
+                    "Absolver",
+                    "Castigator",
+                    "Hallower",
+                    "Scepter",
+                    "Sentinel",
+                    "Sharu",
+                    "Throne"];
+const ajariUnits = ["Sipari",
+                    "Zephyr",
+                    "Saoshin",
+                    "Dervish",
+                    "Absolver",
+                    "Castigator",
+                    "Arc Mother",
+                    "Warden",
+                    "Sentinel",
+                    "Sharu",
+                    "Throne"];
+                    
+const buttonLabels = ["What?",
+                      "Hmmm?",
+                      "Huh?",
+                      "How?",
+                      "That's silly.",
+                      "Ridiculous.",
+                      "Certain death.",
+                      "No way.",
+                      "Not doing it.",
+                      "Never works.",
+                      "Really?",
+                      "Not a chance."];
+
+
+    
+function rand(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+class IGPRandomiser {
+    
+    constructor() {
+    }
+
+    onLoad() {
+        this.immortalBox = document.getElementById("immortalBox");
+        this.playstyleBox = document.getElementById("playstyleBox");
+        this.factionIconBox = document.getElementById("factionIconBox");
+        // //this.question = document.getElementById("question");
+        this.factionIcon = document.getElementById("factionIcon");
+        this.factionLabel = document.getElementById("factionLabel");
+        this.goButton = document.getElementById("button");
+
+        this.buttonTimeout = null;
+        this.unitTimeout = null;
+        this.playstyleTimeout = null;
+        
+        this.clear();
+        this.showStart();
+    }
+        
+    showStart() {
+    }
+    
+    setImmortal(name) {
+        let fadeWrapper = document.createElement("div");
+        fadeWrapper.classList.add("fadeIn");
+        
+        let prefix = document.createElement("p");
+        prefix.classList.add("prefix");
+        prefix.innerHTML = "Play";
+        fadeWrapper.appendChild(prefix);
+        
+        let label = document.createElement("p");
+        label.innerHTML = name;
+        label.classList.add("immortalName");
+        fadeWrapper.appendChild(label);
+        
+        let img = document.createElement("img");
+        img.classList.add("immortalPic");
+        let filename = name.toLowerCase() + ".png";
+        img.src = "images/" + filename;
+        fadeWrapper.appendChild(img);
+        
+        this.immortalBox.appendChild(fadeWrapper);
+    }
+    
+    setUnit(unitname) {
+        let label = document.createElement("p");
+        label.innerHTML = unitname;
+        label.classList.add("unitName");
+        label.classList.add("fadeIn");
+        this.playstyleBox.appendChild(label);
+            
+        let img = document.createElement("img");
+        img.classList.add("unitPic");
+        img.classList.add("fadeIn");
+        let filename = unitname.replace(/ /g,"_").toLowerCase() + ".png";
+        img.src = "images/" + filename;
+        this.playstyleBox.appendChild(img);
+    }
+    
+    setPlaystyle(playstyle) {
+        let prefix = document.createElement("p");
+        prefix.classList.add("prefix");
+        prefix.classList.add("fadeIn");
+        prefix.innerHTML = "And";
+        this.playstyleBox.appendChild(prefix);
+       
+        let label = document.createElement("p");
+        label.innerHTML = playstyle;
+        label.classList.add("playstyleLabel");
+        label.classList.add("fadeIn");
+        this.playstyleBox.appendChild(label);    
+    }
+
+    setFaction(faction) {  
+        // this.question.style.display = "none";
+        this.factionIcon.innerHTML = `<use xlink:href="images/sprite.svg#logo-${faction.toLowerCase()}"></use>`;
+        this.factionIcon.style.display = "block";
+        this.factionLabel.innerHTML = faction;
+        //this.factionLabel.style.display = "block";
+    }
+
+    randomSelection() {
+        let faction = rand(factions);
+        let immortal = "<NOT SET>";
+        let unit = "<NOT SET>";
+        switch (faction) {
+            case "ARU":
+                immortal = rand(aruImmortals);
+                break;
+            case "QRATH":
+                immortal = rand(qrathImmortals);
+                 break;
+            default:
+                console.warn("Unknown faction: " + faction);
+                return;
+        }
+        switch (immortal) {
+            case "ORZUM":
+                unit = rand(orzumUnits);
+                break;
+            case "AJARI":
+                unit = rand(ajariUnits);
+                break;
+            case "MALA":
+                unit = rand(malaUnits);
+                break;
+            case "XOL":
+                unit = rand(xolUnits);
+                break;
+            default:
+                console.warn("Unknown immortal: " + faction);
+                return;
+        }
+        let playstyle = rand(playstyles);
+        return {faction, immortal, unit, playstyle};
+    }
+    
+    buttonClick() {
+        this.clear();
+        this.goButton.innerHTML = "...";
+        setTimeout(()=> {
+            this.chooseRandom();
+            this.buttonTimeout = setTimeout(()=> {
+                this.goButton.innerHTML = rand(buttonLabels);
+            }, 3000);
+        }, 20);
+    }
+
+    chooseRandom() {
+        let choices = this.randomSelection();
+        this.setFaction(choices.faction);
+        this.setImmortal(choices.immortal);
+        this.unitTimeout = setTimeout(()=> {
+            this.setUnit(choices.unit);
+        }, 1000);
+        this.playstyleTimeout = setTimeout(()=> {
+            this.setPlaystyle(choices.playstyle);
+        }, 2000);
+    }
+    
+    clear() {
+        this.factionIcon.style.display = "none";
+        this.factionLabel.style.display = "none";
+        this.playstyleBox.innerHTML = "";
+        this.immortalBox.innerHTML = "";
+        if (this.unitTimeout !== null) {
+            clearTimeout(this.unitTimeout);
+        }
+        if (this.playstyleTimeout !== null) {
+            clearTimeout(this.playstyleTimeout);
+        }
+        if (this.buttonTimeout !== null) {
+            clearTimeout(this.buttonTimeout);
+        }
+    }
+}
+
+randomiser = new IGPRandomiser();
+
+function start() {
+    randomiser.onLoad();
+   
+}
---
+++ _posts/2024-11-24-back-online.md
@@ -0,0 +1,79 @@
+---
+layout: post
+title:  "Back Online"
+date:   2023-08-22 21:41:00 +0000
+categories: news
+---
+It's been quite a year. Or two, really.
+
+I'll be brief for the curious. I'd steadily been getting more unwell for a few years and finally this year got diagnosed with Ideopathic Dumping Syndrome. Stomach not holding onto food, basically.
+
+This made writing hard. It made everything hard.
+
+So I slowed down, and hid from the internet where the brilliant productivity of my indie peers was making it hard to be stoic. And now I'm back! On a doctor diet that has fixed me.
+
+Party horn! 🎉
+
+## Drafting Update
+
+<i>A Murder of Crowns</i> truly is a book born from chaos. It was two books, then one, then something I was writing while sick. If you liked how peculiar <i>A Handful of Souls</i> was, you're in for a treat.
+
+Generally, I build a lot of complexity in my writing, then make it breezier by layering on carefully offhand character work/pacing. But bloody hell, this book has taken some layering.
+
+I sort of like it now.
+
+But I'm not going to move forwards until it's enjoyable to read for the readers that just want to visit neat fantasy cities and enjoy light-hearted banter  punctuated by horror tropes and troubling life choices.
+
+For those readers that enjoy rooting around in the details and being the Right Kind of confused... well, for some ridiculous reason I made the dominant sub plot of this book a murder mystery.
+
+This has made it decidely hard to cater to both reader groups, because FOR THE LOVE OF GOD, fantasy murder mystery is probably avoided for good reason. I'm having to explain a different world as well as trying to leave the reader clues - it really drags the pacing down if you let it.
+
+But we only do honest murders here. I'll be pleased if people figure out who the scoundrels are before they're revealed. After all, who could murder a feaster? Must be obvious, right?
+
+## When book?
+
+I'm not giving deadlines for now.
+
+Safe to say <i>A Murder of Crowns</i> is still months away, as once I'm done with personal drafts we have to go on to beta readers, edit, cover design, interior etc. But I solemnly assure you I'm working hard and loving it.
+
+
+
+## Other Things
+
+It rather bothers me that I had found a bunch of communities and friendships online, then had to retreat from them. Essentially, being an indie author is as much about being visible as it is about writing.
+
+And I've been hiding.
+
+To that end, I'm maybe going to be dabbling in:
+ 
+- Eccentric writing update videos
+- Streaming (maybe writing sprints, maybe gaming, maybe both)
+- More regular social media posts
+
+My empty youtube channel is <a href="https://www.youtube.com/@writing_steve">here</a>. I'm going to fill it with things that bring me joy. My twitter is <a href="https://twitter.com/writing_steve">here</a>, where I will post a variety of nonsense
+
+
+
+## What's To Come
+
+Aside from <i>A Murder of Crowns</i>, I'm progressing with ideas to publish it alongside a second edition of <i>A Handful of Souls</i>. Essentially, I now know self-publishing a little better, and want a second stab at covers and interiors. I'm also (maybe) contemplating adding a few paragraphs to ease some of the cold transitions, as they were the most consistent struggle-feedback I received.
+
+But that said, I'm so deeply pleased with the positive reception to <i>A Handful of Souls</i>. I just threw it out there, thinking I could then work on my writing in peace, but people are still contacting me with encouraging words, and some sales trickle on despite me not having paid for any advertising, ever.
+
+That will come. The idea is still to 'launch' with two books when MoC is ready. I've got a few neat ideas re: interiors as well, so watch this space.
+
+## Steve's Todo List
+
+1. <i>A Murder of Crowns</i> draft (90%)
+2. Beta readers and Editing (20%)
+3. Second Edition HoS Covers/Interiors/Surprises (0%)
+4. Prequel Novella for Dren/Terrano/Husker (5%)
+5. Make eccentric update vlog (110%)
+
+You can see that first vlog <a href="https://www.youtube.com/watch?v=Ubb3aonoi_Y">here</a>.
+
+## Writing Steve's Grand Returning Sale
+
+Lastly, I'll be running a sale on <i>A Handful of Souls</i> during the sacred and frenzied ritual nights of Black Friday. Just <span style="color: #fbda4a">£0.99</span> ($/€) for a 'strange, beguiling story' (kind words provided from the most recent amazon review).
+
+Until next time!
